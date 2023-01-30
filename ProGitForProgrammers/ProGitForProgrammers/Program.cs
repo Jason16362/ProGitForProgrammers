674aaa097672bbb8a0aa8db46faacd8484208d40
Jason Chung <jason16362@gmail.com>
2023-01-30 22:55:11 +0800

    Visual Studio - Show Staging Area

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio!");
 Console.WriteLine("I just added this in Command Line");
 Console.WriteLine("This line added to show staging area");
+Console.WriteLine("This line added to show staging area - from Visual Studio");
+
